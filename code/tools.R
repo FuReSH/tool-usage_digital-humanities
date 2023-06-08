6f825775652b69bc2be8e71b7cc5034a8a3d92ac
Till Grallert <till.grallert@fu-berlin.de>
2023-06-08 23:21:01 +0200

    renamed data frames

--- code/tools.R
+++ code/tools.R
@@ -15,7 +15,7 @@ read_csv("tadirah/tadirah-wikidata.csv") %>%
                 tadirah.id = label) %>%
   # remove all QIds that have been deleted from the Wikidata
   dplyr::mutate(wd.item = ifelse(wd.deleted == T, NA, wd.item)) %>%
-  dplyr::select(tadirah.id, tadirah.uri, wd.item) -> data.tools.tadirah.wd
+  dplyr::select(tadirah.id, tadirah.uri, wd.item) -> df.tools.tadirah.wd
 
 # SSH Open Marketplace with TaDiRAH classification
 read_csv("ssh-open-marketplace/ssh_tools-classification.csv") %>%
@@ -26,20 +26,20 @@ read_csv("ssh-open-marketplace/ssh_tools-classification.csv") %>%
                 tadirah.id = concept.code,
                 tadirah.uri = concept.uri) %>%
   dplyr::select(ssh.id, ssh.label,tadirah.id, tadirah.uri) %>%
-  dplyr::distinct() -> data.tools.ssh.tadirah
+  dplyr::distinct() -> df.tools.ssh.tadirah
 
 # load the full SSH tool list
 load("ssh-open-marketplace/ssh.rda") 
 ## df with mapping from SSH to TAPoR
-data.ssh %>%
+df.ssh %>%
   tibble::as_tibble() %>%
   dplyr::mutate(tapor.id = ifelse(source.label == "TAPoR", sourceItemId, NA)) %>% # include TAPoR Id
   dplyr::rename(ssh.id = persistentId,
                 ssh.label = label,
                 ssh.desc = description) %>%
   dplyr::mutate(ssh.label = str_replace(ssh.label, '^\\s+', '')) %>%
-  dplyr::select(ssh.id, ssh.label, ssh.desc, tapor.id)-> data.tools.ssh.description
-remove(data.ssh)
+  dplyr::select(ssh.id, ssh.label, ssh.desc, tapor.id)-> df.tools.ssh.description
+remove(df.ssh)
 
 # clean the labels, which contain a lot of information, such as accronyms etc.
 # function to apply a regex search and replace on the ssh.label field
@@ -54,45 +54,50 @@ f.get.abbr <- function(df.input, v.regex, min.length){
                                           ssh.label.abbr,
                                           NA)) 
 }
-data.ssh.labels <- data.tools.ssh.description %>%
+df.ssh.labels <- df.tools.ssh.description %>%
   dplyr::mutate(ssh.label.abbr = NA) %>%
   dplyr::select(ssh.id, ssh.label, ssh.label.abbr, ssh.desc, tapor.id)
 v.regex.1 = c('^.*\\((\\w{2,})\\).*$') # find all single words in parentheses
 v.regex.2  = c('^.*\\b([A-Z]{3,})\\b.*$') # find all words in uppercase and min 3 letters long
 v.regex.3 = c('^.*?(([A-Z]+[a-z]*){2,}).*?$') # camel case
 v.regex.4 =c('^.*?([A-z]+\\d+[A-z]+).*?$') # character strings with numbers somewhere in between
-data.ssh.labels %>%
+df.ssh.labels %>%
   f.get.abbr(v.regex.4, 4) %>%
   f.get.abbr(v.regex.3, 3) %>%
   f.get.abbr(v.regex.2, 3) %>%
-  f.get.abbr(v.regex.1, 3) -> data.ssh.labels # this is only for testing. ultimately results can be directly written back to the source
-data.ssh.labels -> data.tools.ssh.description
+  f.get.abbr(v.regex.1, 3) -> df.ssh.labels # this is only for testing. ultimately results can be directly written back to the source
+df.ssh.labels -> df.tools.ssh.description
 
 # df with mapping from TAPoR to Wikidata
 load("tapor/tapor_tools-wikidata.rda")
-data.tapor.wikidata %>%
+df.tapor.wikidata %>%
   dplyr::rename(tapor.id = id.tapor,
                 tapor.label = label,
                 tapor.desc = description) %>%
   dplyr::select(!label.clean) %>%
-  dplyr::mutate(tapor.id = as.character(tapor.id)) -> data.tools.tapor.wd
-remove(data.tapor.wikidata)
+  dplyr::mutate(tapor.id = as.character(tapor.id)) -> df.tools.tapor.wd
+remove(df.tapor.wikidata)
 
 # join tool lists
-full_join(data.tools.ssh.tadirah, data.tools.tadirah.wd) %>%
-  dplyr::rename(wd.tadirahid = wd.item) -> data.tools.classification.wd
+full_join(df.tools.ssh.tadirah, df.tools.tadirah.wd) %>%
+  dplyr::rename(wd.tadirahid = wd.item) -> df.tools.classification.wd
 # all tool information without TaDiRAH
-full_join(data.tools.ssh.description, data.tools.tapor.wd) -> data.tools.wd
+full_join(df.tools.ssh.description, df.tools.tapor.wd) -> df.tools.wd
+# get short names for those tools already reconciled with Wikidata
+df.tools.wd %>%
+  dplyr::mutate(wd.label = ifelse(is.na(wd.item) == T,
+                                  NA,
+                                  f.wikidata.properties(wd.item, "P1813"))) -> df.tools.wd
 
 # add everything together
-left_join(data.tools.wd, data.tools.classification.wd) %>%
-  dplyr::select(ssh.id, tapor.id, ssh.label, ssh.label.abbr, tapor.label, ssh.desc, tapor.desc, tadirah.id, tadirah.uri, wd.tadirahid) -> data.tools.everything
+left_join(df.tools.wd, df.tools.classification.wd) %>%
+  dplyr::select(ssh.id, tapor.id, ssh.label, ssh.label.abbr, tapor.label, ssh.desc, tapor.desc, tadirah.id, tadirah.uri, wd.tadirahid) -> df.tools.everything
 
 # save joined dfs
-save(data.tools.wd, file = "tools_ssh-tapor-wd.rda")
-write.table(data.tools.wd, file = "tools_ssh-tapor-wd.csv", row.names = F, sep = ",")
-save(data.tools.classification.wd, file = "tools_tadirah-wd.rda")
-save(data.tools.everything, file = "tools_ssh-tapor-wd-tadirah.rda")
-write.table(data.tools.everything, file = "tools_ssh-tapor-wd-tadirah.csv", row.names = F, sep = ",")
+save(df.tools.wd, file = "tools_ssh-tapor-wd.rda")
+write.table(df.tools.wd, file = "tools_ssh-tapor-wd.csv", row.names = F, sep = ",")
+save(df.tools.classification.wd, file = "tools_tadirah-wd.rda")
+save(df.tools.everything, file = "tools_ssh-tapor-wd-tadirah.rda")
+write.table(df.tools.everything, file = "tools_ssh-tapor-wd-tadirah.csv", row.names = F, sep = ",")
 
 
